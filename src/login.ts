f810c53bd978c4ca96b0ff6ee6c55b9906146613
joaobatistamedeiroscf <joaobatistamedeiroscf@gmail.com>
2025-08-08 21:57:50 -0300

    adicionando validações ao formulario de cadastro

---
+++ src/login.ts
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("login-form") as HTMLFormElement;
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const email = (document.getElementById("login-email") as HTMLInputElement).value.trim();
+    const password = (document.getElementById("login-senha") as HTMLInputElement).value;
+
+    const storedUser = localStorage.getItem("user");
+
+    if (!storedUser) {
+      alert("Nenhum usuário cadastrado!");
+      return;
+    }
+
+    const user = JSON.parse(storedUser);
+
+    if (email === user.email && password === user.password) {
+      alert(`Bem-vindo, ${user.name}!`);
+      window.location.href = "/src/paginaprincipal.html"; // depois criaremos
+    } else {
+      alert("Email ou senha inválidos!");
+    }
+  });
+});
---
+++ src/register.ts
@@ -0,0 +1,49 @@
+import { campoVazio, validaNome, validaEmail, validaSenha, confirmaSenha } from "./registervalidation";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("register-form") as HTMLFormElement;
+
+  form.addEventListener("submit", (evento) => {
+    evento.preventDefault();
+    console.log("Botão sendo clicado")!
+
+    const name = (document.getElementById("register-name") as HTMLInputElement).value;
+    const email = (document.getElementById("register-email") as HTMLInputElement).value;
+    const birth = (document.getElementById("register-nascimento") as HTMLInputElement).value;
+    const password = (document.getElementById("register-senha") as HTMLInputElement).value;
+    const confirmPassword = (document.getElementById("register-conf-senha") as HTMLInputElement).value;
+
+    const msgNome = document.getElementById("erro-name") as HTMLParagraphElement;
+    const msgEmail = document.getElementById("erro-email") as HTMLParagraphElement;
+    const msgNascimento = document.getElementById("erro-nascimento") as HTMLParagraphElement;
+    const msgSenha = document.getElementById("erro-senha") as HTMLParagraphElement;
+    const msgConfSenha = document.getElementById("erro-conf-senha") as HTMLParagraphElement;
+
+    
+    let valido = true;
+
+    if (campoVazio(name, msgNome, "Preencha o nome!")) valido = false;
+    else if (!validaNome(name, msgNome)) valido = false;
+
+    if (campoVazio(email, msgEmail, "Preencha o email!")) valido = false;
+    else if (!validaEmail(email, msgEmail)) valido = false;
+
+    if (campoVazio(birth, msgNascimento, "Preencha a data de nascimento!")) valido = false;
+
+    if (campoVazio(password, msgSenha, "Preencha a senha!")) valido = false;
+    else if (!validaSenha(password, msgSenha)) valido = false;
+
+    if (campoVazio(confirmPassword, msgConfSenha, "Confirme a senha!")) valido = false;
+    else if (!confirmaSenha(password, confirmPassword, msgConfSenha)) valido = false;
+
+    if (!valido) return;
+
+  
+    const user = { name, email, birth, password };
+    localStorage.setItem("user", JSON.stringify(user));
+
+
+    alert("✅ Cadastro realizado com sucesso!");
+    window.location.href = "/index.html";
+  });
+});
